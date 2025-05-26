15eb32b413d501ecbf9d9a5ec8c7a14f448ee97f
K20QuickFast237 <nanguedevops@gmail.com>
2025-05-26 07:59:57 +0100

    included CREATE TABLE in partitioning script

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,7 +1,25 @@
 -- Implement partitioning on the Booking table based on the start_date column
-ALTER TABLE `booking` PARTITION BY RANGE COLUMNS (start_date) (
+-- First Try
+ PARTITION BY RANGE COLUMNS (start_date) (
     PARTITION p0 VALUES LESS THAN ('2023-01-01'),
     PARTITION p1 VALUES LESS THAN ('2024-01-01'),
     PARTITION p2 VALUES GREATER THAN ('2024-12-31')
 );
 
+-- Second Try
+CREATE TABLE `booking` (
+    `booking_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `property_id` INT NOT NULL,
+    `user_id` INT NOT NULL COMMENT 'The user who made the booking (guest)',
+    `start_date` DATE NOT NULL,
+    `end_date` DATE NOT NULL,
+    `total_price` DECIMAL(12, 2) NOT NULL CHECK (`total_price` >= 0),
+    `status` ENUM('pending_confirmation', 'confirmed', 'cancelled_by_user', 'cancelled_by_host', 'completed', 'no_show') NOT NULL DEFAULT 'pending_confirmation',
+    `number_of_guests` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+PARTITION BY RANGE COLUMNS (start_date) (
+    PARTITION p0 VALUES LESS THAN ('2023-01-01'),
+    PARTITION p1 VALUES LESS THAN ('2024-01-01')
+);
